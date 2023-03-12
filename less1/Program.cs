3cd80f23fcf2761fee609e5b5214f90b86975939
Sergey Atepalikhin <vinyl51@ya.ru>
2023-03-12 19:08:47 +0300

    delenie/2

--- less1/Program.cs
+++ less1/Program.cs
@@ -1,3 +1,25 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 //5 -> 2, 4
-//8 -> 2, 4, 6, 8
+//8 -> 2, 4, 6, 8 
+
+
+int n = 0;
+
+Console.WriteLine("Введите число: ");
+string vvod = Console.ReadLine()!;
+n = int.Parse(vvod);
+
+int delenie = 2;
+
+
+while (delenie <=n)
+{
+  if (delenie+1>=n)
+    Console.Write($"{delenie}");
+  else
+    Console.Write($"{delenie}, ");
+  delenie +=2;
+  
+}
+
+
