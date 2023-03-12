bc151e32c46cb6f8d0d204335ee67b624634784b
Sergey Atepalikhin <vinyl51@ya.ru>
2023-03-12 18:00:27 +0300

    all

--- less1/Program.cs
+++ less1/Program.cs
@@ -3,3 +3,15 @@
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
+int a;
+int b;
+
+Console.Write("Add number1: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write("Add number2: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+{
+
+}
+
